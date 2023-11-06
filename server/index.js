441e60551d68bae306d208d04b41548c7f06f609
rana <rana.ahammed.012@gmail.com>
2023-11-06 10:38:10 +0600

    .

--- server/index.js
+++ server/index.js
@@ -30,6 +30,8 @@ app.get("/", (req, res) => {
 	res.send("Server is working");
 });
 
-app.listen(process.env.PORT, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
